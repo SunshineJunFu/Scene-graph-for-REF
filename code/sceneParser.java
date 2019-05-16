7ce8319fc9cda3e9e951b2501c1cc0aeade4d175
Jun Fu <33153297+SunshineJunFu@users.noreply.github.com>
2019-05-16 22:13:21 +0800

    Add files via upload

---
+++ code/sceneParser.java
@@ -0,0 +1,16 @@
+import edu.stanford.nlp.scenegraph.RuleBasedParser;
+import java.io.*;
+import edu.stanford.nlp.scenegraph.SceneGraph;
+public class sceneParser{
+    public static void main(String[] args)throws FileNotFoundException {
+    String sentence = args[0]; //"A brown fox chases a white rabbit";
+
+    RuleBasedParser parser = new RuleBasedParser();
+    SceneGraph sg = parser.parse(sentence);
+    System.out.println(sg.toReadableString());
+    //sg.toJSONString();
+    PrintWriter pw = new PrintWriter(new File(args[1]+".txt"));
+    pw.println(sg.toReadableString());
+    pw.close();
+}
+}
